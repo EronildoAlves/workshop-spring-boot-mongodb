ed55fe1db0d26c570848e8e1a99ece8beadce032
Eronildo Nascimento <eronildof36@gmail.com>
2024-09-28 19:16:38 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/eronildo/workshop/services/UserService.java
+++ src/main/java/com/eronildo/workshop/services/UserService.java
@@ -16,20 +16,25 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.insert(obj);
 	}
-	
+
+	public void delete(String id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
